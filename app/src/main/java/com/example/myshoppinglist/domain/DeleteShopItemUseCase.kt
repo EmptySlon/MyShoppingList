bc294052e943f29390a8bb9199dc91ec78e37a0b
empty <empty87@yandex.ru>
2022-11-07 16:33:30 +0300

    first commit

---
+++ app/src/main/java/com/example/myshoppinglist/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.myshoppinglist.domain
+
+class DeleteShopItemUseCase {
+    fun deleteShopItem(shopItem: ShopItem) {
+
+    }
+}
---
+++ app/src/main/java/com/example/myshoppinglist/domain/GetShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.myshoppinglist.domain
+
+class GetShopItemUseCase {
+    fun getShopList(shopItemId: Int): ShopItem {
+        TODO()
+    }
+}

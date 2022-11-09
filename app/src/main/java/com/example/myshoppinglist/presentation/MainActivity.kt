5a4e12eae79a21cd1a0d26d0c03bac35ada47d2e
empty <empty87@yandex.ru>
2022-11-09 22:29:04 +0300

    lesson 7

--- app/src/main/java/com/example/myshoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myshoppinglist/presentation/MainActivity.kt
@@ -7,47 +7,30 @@ import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myshoppinglist.R
 import com.example.myshoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var llListShopItem: LinearLayout
-
-    private var count = 0
-
+    lateinit var adapter: ShopListAdapter
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        llListShopItem = findViewById(R.id.ll_shop_list)
-
+        setupRecycleView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this){
-            llListShopItem.removeAllViews()
-            for (shopItem in it) {
-                val layoutId = if (shopItem.enabled) R.layout.item_shop_enabled
-                else R.layout.item_shop_disabled
-
-                val view = LayoutInflater.from(this).inflate(layoutId,llListShopItem,false )
-                val tvName = view.findViewById<TextView>(R.id.tv_name)
-                val tvCount = view.findViewById<TextView>(R.id.tv_count)
-                tvName.text = shopItem.name
-                tvCount.text = shopItem.count.toString()
-
-                view.setOnLongClickListener {
-                    viewModel.changeEnabled(shopItem)
-                    true
-                }
-
-                llListShopItem.addView(view)
-
-            }
-
+            adapter.shopList = it
 
         }
 
+    }
 
+    private fun setupRecycleView(){
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        adapter = ShopListAdapter()
+        rvShopList.adapter = adapter
     }
 }
